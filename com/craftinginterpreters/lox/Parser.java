30fb6c9fff13ceb4527250902fa7db50710f1848
Scott <dscottwiggins@gmail.com>
2020-01-02 19:09:02 -0500

    finish java and try getting c stuff running

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -1,5 +1,7 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.craftinginterpreters.lox.TokenType.*;
@@ -15,25 +17,242 @@ class Parser {
     this.tokens = tokens;
   }
 
-  Expr parse() {
+  List<Stmt> parse() {
+    List<Stmt> statements = new ArrayList<>();
+    while (!isAtEnd()) {
+      statements.add(declaration());
+    }
+
+    return statements;
+  }
+
+  private Stmt declaration() {
     try {
-      return expression();
+      if (match(CLASS))
+        return classDeclaration();
+      if (match(FUN))
+        return function("function");
+      if (match(VAR))
+        return varDeclaration();
+
+      return statement();
     } catch (ParseError error) {
+      synchronize();
       return null;
     }
   }
 
-  private Expr expression() {
-    return comma();
+  private Stmt classDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect class name");
+
+    Expr.Variable superclass = null;
+    if (match(LESS)) {
+      consume(IDENTIFIER, "Expect superclass name");
+      superclass = new Expr.Variable(previous());
+    }
+
+    consume(LEFT_BRACE, "Expect '{' before class body");
+
+    List<Stmt.Function> methods = new ArrayList<>();
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      methods.add(function("method"));
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after class body");
+
+    return new Stmt.Class(name, superclass, methods);
   }
 
-  private Expr comma() {
+  private Stmt statement() {
+    if (match(FOR))
+      return forStatement();
+    if (match(IF))
+      return ifStatement();
+    if (match(PRINT))
+      return printStatement();
+    if (match(RETURN))
+      return returnStatement();
+    if (match(WHILE))
+      return whileStatement();
+    if (match(LEFT_BRACE))
+      return new Stmt.Block(block());
+
+    return expressionStatement();
+  }
+
+  private Stmt forStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'for'");
+
+    Stmt initializer;
+    if (match(SEMICOLON)) {
+      initializer = null;
+    } else if (match(VAR)) {
+      initializer = varDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+
+    Expr condition = null;
+    if (!check(SEMICOLON)) {
+      condition = expression();
+    }
+    consume(SEMICOLON, "Expect ';' after loop condition");
+
+    Expr increment = null;
+    if (!check(RIGHT_PAREN)) {
+      increment = expression();
+    }
+    consume(RIGHT_PAREN, "Expect ')' after for clauses");
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+    }
+
+    if (condition == null)
+      condition = new Expr.Literal(true);
+    body = new Stmt.While(condition, body);
+
+    if (initializer != null) {
+      body = new Stmt.Block(Arrays.asList(initializer, body));
+    }
+
+    return body;
+  }
+
+  private Stmt ifStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'if'");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expect ')' after if condition");
+
+    Stmt thenBranch = statement();
+    Stmt elseBranch = null;
+    if (match(ELSE)) {
+      elseBranch = statement();
+    }
+
+    return new Stmt.If(condition, thenBranch, elseBranch);
+  }
+
+  private Stmt printStatement() {
+    Expr value = expression();
+    consume(SEMICOLON, "Expect ';' after value");
+    return new Stmt.Print(value);
+  }
+
+  private Stmt returnStatement() {
+    Token keyword = previous();
+    Expr value = null;
+    if (!check(SEMICOLON)) {
+      value = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after return value");
+    return new Stmt.Return(keyword, value);
+  }
+
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name");
+
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after variable declaration");
+    return new Stmt.Var(name, initializer);
+  }
+
+  private Stmt whileStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'while'");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expect ')' after condition");
+    Stmt body = statement();
+
+    return new Stmt.While(condition, body);
+  }
+
+  private Stmt expressionStatement() {
+    Expr expr = expression();
+    consume(SEMICOLON, "Expect ';' after expression");
+    return new Stmt.Expression(expr);
+  }
+
+  private Stmt.Function function(String kind) {
+    Token name = consume(IDENTIFIER, "Expect " + kind + " name");
+    consume(LEFT_PAREN, "Expect '(' after " + kind + " name");
+    List<Token> parameters = new ArrayList<>();
+    if (!check(RIGHT_PAREN)) {
+      do {
+        if (parameters.size() >= 255) {
+          error(peek(), "Cannot have more than 255 parameters");
+        }
+
+        parameters.add(consume(IDENTIFIER, "Expect parameter name"));
+      } while (match(COMMA));
+    }
+    consume(RIGHT_PAREN, "Expect ')' after parameters");
+
+    consume(LEFT_BRACE, "Expect '{' before " + kind + "body");
+    List<Stmt> body = block();
+    return new Stmt.Function(name, parameters, body);
+  }
+
+  private List<Stmt> block() {
+    List<Stmt> statements = new ArrayList<>();
+
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      statements.add(declaration());
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after block");
+    return statements;
+  }
+
+  private Expr expression() {
+    return assignment();
+  }
+
+  private Expr assignment() {
+    Expr expr = or();
+
+    if (match(EQUAL)) {
+      Token equals = previous();
+      Expr value = assignment();
+
+      if (expr instanceof Expr.Variable) {
+        Token name = ((Expr.Variable) expr).name;
+        return new Expr.Assign(name, value);
+      } else if (expr instanceof Expr.Get) {
+        Expr.Get get = (Expr.Get) expr;
+        return new Expr.Set(get.object, get.name, value);
+      }
+
+      error(equals, "Invalid assignment target");
+    }
+
+    return expr;
+  }
+
+  private Expr or() {
+    Expr expr = and();
+
+    while (match(OR)) {
+      Token operator = previous();
+      Expr right = and();
+      expr = new Expr.Logical(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr and() {
     Expr expr = equality();
 
-    while (match(COMMA)) {
+    while (match(AND)) {
       Token operator = previous();
       Expr right = equality();
-      expr = new Expr.Binary(expr, operator, right);
+      expr = new Expr.Logical(expr, operator, right);
     }
 
     return expr;
@@ -94,7 +313,40 @@ class Parser {
       return new Expr.Unary(operator, right);
     }
 
-    return primary();
+    return call();
+  }
+
+  private Expr call() {
+    Expr expr = primary();
+
+    while (true) {
+      if (match(LEFT_PAREN)) {
+        expr = finishCall(expr);
+      } else if (match(DOT)) {
+        Token name = consume(IDENTIFIER, "Expect property name after '.'");
+        expr = new Expr.Get(expr, name);
+      } else {
+        break;
+      }
+    }
+
+    return expr;
+  }
+
+  private Expr finishCall(Expr callee) {
+    List<Expr> arguments = new ArrayList<>();
+    if (!check(RIGHT_PAREN)) {
+      do {
+        if (arguments.size() >= 255) {
+          error(peek(), "Cannot have more than 255 arguments");
+        }
+        arguments.add(expression());
+      } while (match(COMMA));
+    }
+
+    Token paren = consume(RIGHT_PAREN, "Expect ')' after arguments");
+
+    return new Expr.Call(callee, paren, arguments);
   }
 
   private Expr primary() {
@@ -107,6 +359,20 @@ class Parser {
 
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+
+    if (match(SUPER)) {
+      Token keyword = previous();
+      consume(DOT, "Expect '.' after 'super'");
+      Token method = consume(IDENTIFIER, "Expect superclass method name");
+      return new Expr.Super(keyword, method);
+    }
+
+    if (match(THIS))
+      return new Expr.This(previous());
+
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
 
     if (match(LEFT_PAREN)) {
--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -13,8 +13,20 @@ public class GenerateAst {
     }
 
     String outputDir = args[0];
-    defineAst(outputDir, "Expr", Arrays.asList("Binary : Expr left, Token operator, Expr right",
-        "Grouping : Expr expression", "Literal : Object value", "Unary : Token operator, Expr right"));
+    defineAst(outputDir, "Expr",
+        Arrays.asList("Assign : Token name, Expr value", "Binary : Expr left, Token operator, Expr right",
+            "Call : Expr callee, Token paren, List<Expr> arguments", "Get : Expr object, Token name",
+            "Grouping : Expr expression", "Literal : Object value", "Logical : Expr left, Token operator, Expr right",
+            "Set : Expr object, Token name, Expr value", "Super : Token keyword, Token method", "This : Token keyword",
+            "Unary : Token operator, Expr right", "Variable : Token name"));
+
+    defineAst(outputDir, "Stmt",
+        Arrays.asList("Block : List<Stmt> statements",
+            "Class : Token name, Expr.Variable superclass, List<Stmt.Function> methods", "Expression : Expr expression",
+            "Function : Token name, List<Token> params, List<Stmt> body",
+            "If : Expr condition, Stmt thenBranch, Stmt elseBranch", "Print : Expr expression",
+            "Return : Token keyword, Expr value", "Var : Token name, Expr initializer",
+            "While : Expr condition, Stmt body"));
   }
 
   private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
